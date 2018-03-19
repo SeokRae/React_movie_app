028151a8ff541a366ee403712d82eb08a2b9a455
SEOKRAE\SeokRae <kslbsh@gmail.com>
2018-03-20 00:43:41 +0900

    Commit Giving some CSS to

--- src/App.js
+++ src/App.js
@@ -16,24 +16,25 @@ class App extends Component {
   _renderMovies = () => {
     const movies = this.state.movies.map(movie => {
       console.log(movie)
-      return <Movie 
-        title={movie.title_english} 
-        poster={movie.medium_cover_image} 
-        key={movie.id} 
-        genres={movie.genres} 
-        synopsis={movie.synopsis} 
-      />;
-
+      return (
+        <Movie 
+          title={movie.title_english} 
+          poster={movie.medium_cover_image} 
+          key={movie.id} 
+          genres={movie.genres} 
+          synopsis={movie.synopsis} 
+        />
+      );
     });
-    return movies
+    return movies;
   }
 
   _getMovies = async () => {
     const movies = await this._callApi();
     this.setState({
       movies
-    })
-  }
+    });
+  };
 
   _callApi = () => {
     return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
@@ -43,9 +44,10 @@ class App extends Component {
   };
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : "Loading"}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
